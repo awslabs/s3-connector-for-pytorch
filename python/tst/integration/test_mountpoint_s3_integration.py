ffdbeacb44f845222b27232ac32837de791144ae
dnnanuti <dnnanuti@amazon.com>
2023-10-19 15:08:05 +0100

    Fix formatting

--- python/tst/integration/test_mountpoint_s3_integration.py
+++ python/tst/integration/test_mountpoint_s3_integration.py
@@ -2,7 +2,9 @@ import logging
 
 from s3dataset._s3dataset import MountpointS3Client
 
-logging.basicConfig(format="%(levelname)s %(name)s %(asctime)-15s %(filename)s:%(lineno)d %(message)s")
+logging.basicConfig(
+    format="%(levelname)s %(name)s %(asctime)-15s %(filename)s:%(lineno)d %(message)s"
+)
 logging.getLogger().setLevel(1)
 
 log = logging.getLogger(__name__)
@@ -22,16 +24,19 @@ def test_list_objects():
 
     object_infos = [object_info for page in stream for object_info in page.object_info]
     keys = {object_info.key for object_info in object_infos}
-    assert keys == {"iterable-datasets/",
-                    "iterable-datasets/num-iterdataset-100.pkl",
-                    "iterable-datasets/pickle-num-iterdataset-100.pkl",
-                    "iterable-datasets/pickle-str-iterdataset-10.pkl",
-                    "iterable-datasets/str-iterdataset-10.pkl",
-                    "iterable-datasets/torch-num-iterdataset-100.pkl",
-                    "iterable-datasets/torch-str-iterdataset-10.pkl",
-                    "sample-files/",
-                    "sample-files/catalog.csv",
-                    "sample-files/hello_world.txt"}
+    assert keys == {
+        "iterable-datasets/",
+        "iterable-datasets/num-iterdataset-100.pkl",
+        "iterable-datasets/pickle-num-iterdataset-100.pkl",
+        "iterable-datasets/pickle-str-iterdataset-10.pkl",
+        "iterable-datasets/str-iterdataset-10.pkl",
+        "iterable-datasets/torch-num-iterdataset-100.pkl",
+        "iterable-datasets/torch-str-iterdataset-10.pkl",
+        "sample-files/",
+        "sample-files/catalog.csv",
+        "sample-files/hello_world.txt",
+    }
+
 
 def test_list_objects_with_prefix():
     client = MountpointS3Client("eu-west-2")
@@ -39,8 +44,8 @@ def test_list_objects_with_prefix():
 
     object_infos = [object_info for page in stream for object_info in page.object_info]
     keys = {object_info.key for object_info in object_infos}
-    assert keys == {"sample-files/",
-                    "sample-files/catalog.csv",
-                    "sample-files/hello_world.txt"}
-
-
+    assert keys == {
+        "sample-files/",
+        "sample-files/catalog.csv",
+        "sample-files/hello_world.txt",
+    }
