c65b0cb3dd40905e12511df7ceedc95dc3295d37
dnnanuti <dnnanuti@amazon.com>
2023-10-23 16:40:13 +0100

    Update docs

--- python/src/s3dataset/s3object.py
+++ python/src/s3dataset/s3object.py
@@ -1,7 +1,10 @@
 import io
 from s3dataset._s3dataset import ObjectInfo, GetObjectStream
 
-
+"""
+s3_object.py
+    File like representation of an S3 object.
+"""
 class S3Object(io.BufferedIOBase):
     def __init__(
         self,
