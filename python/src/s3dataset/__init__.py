edb69eca1131d50b95e7d58f35f255320d3a8a2a
Simon Beal <simobeal@amazon.com>
2023-10-25 16:25:28 +0100

    Add public exports to s3dataset module
    Adds `S3Object`, `S3IterableDataset`,
    and `S3DatasetBase` as exports.

--- python/src/s3dataset/__init__.py
+++ python/src/s3dataset/__init__.py
@@ -1,8 +1,14 @@
-from s3dataset._logger_patch import TRACE as LOG_TRACE
-from s3dataset._logger_patch import _install_trace_logging
+from ._logger_patch import TRACE as LOG_TRACE
+from ._logger_patch import _install_trace_logging
+from .s3dataset_base import S3DatasetBase
+from .s3iterable_dataset import S3IterableDataset
+from .s3object import S3Object
 
 _install_trace_logging()
 
 __all__ = [
     "LOG_TRACE",
+    "S3Object",
+    "S3IterableDataset",
+    "S3DatasetBase",
 ]
