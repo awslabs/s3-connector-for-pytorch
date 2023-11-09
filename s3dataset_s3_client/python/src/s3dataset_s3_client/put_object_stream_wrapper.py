a9d4f5811f01567d523c0034a8e529f7c9c526de
Simon Beal <5381483+muddyfish@users.noreply.github.com>
2023-11-09 10:36:46 +0000

    Implement basic checkpointing support (#25)
    * Implement basic checkpointing support
    
    Add support for `torch.load` and `torch.save` for objects in S3
    Add tests for `torch.load` and `torch.save`
    
    Add support for `S3Object.seek` using a `BytesIO` buffer under the hood.
    Add support to `S3Object.read()` to specify the amount of bytes to read. Only blocks on S3 for the chunks needed to fulfill the read request.
    
    Add tests for `S3Object.seek` and `S3Object.read`
    Add doc comments for `read` and `seek`

---
+++ s3dataset_s3_client/python/src/s3dataset_s3_client/put_object_stream_wrapper.py
@@ -0,0 +1,25 @@
+from typing import Union
+
+from s3dataset_s3_client._s3dataset import PutObjectStream
+
+
+class PutObjectStreamWrapper:
+    def __init__(self, stream: PutObjectStream):
+        self.stream = stream
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def write(self, data: Union[bytes, memoryview]):
+        if isinstance(data, memoryview):
+            data = data.tobytes()
+        self.stream.write(data)
+
+    def close(self):
+        self.stream.close()
+
+    def flush(self):
+        pass
