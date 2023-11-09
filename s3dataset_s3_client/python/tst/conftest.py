9032a9a74f4390fe0b6058a4b317e155135c5f5a
Simon Beal <simobeal@amazon.com>
2023-11-09 13:44:00 +0000

    Fix conftest.py

--- s3dataset_s3_client/python/tst/conftest.py
+++
@@ -1,7 +0,0 @@
-import os
-from datetime import timedelta
-
-from hypothesis import settings
-
-settings.register_profile("ci", max_examples=1000, deadline=timedelta(seconds=1))
-settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "default"))
