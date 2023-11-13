44af7d2e8745589c3cfe071d925b49f34afcff6e
Simon Beal <simobeal@amazon.com>
2023-11-13 14:52:53 +0000

    Multiprocess tests now verify the object was downloaded from S3 correctly

--- s3dataset/tst/e2e/test_multiprocess_dataloading.py
+++ s3dataset/tst/e2e/test_multiprocess_dataloading.py
@@ -1,3 +1,5 @@
+import os
+import re
 from collections import Counter
 
 from s3dataset_s3_client._s3dataset import MountpointS3Client
@@ -9,6 +11,10 @@ from s3dataset_s3_client import S3Object
 E2E_TEST_BUCKET = "dataset-it-bucket"
 E2E_BUCKET_PREFIX = "e2e-tests/images-10/img"
 E2E_TEST_REGION = "eu-west-2"
+LOCAL_DATASET_RELATIVE_PATH = "../resources/images-10/"
+ABSOLUTE_PATH = os.path.dirname(__file__)
+
+image_key_re = re.compile(r"img\d+\.jpg$")
 
 
 def test_s3iterable_dataset_multiprocess():
@@ -18,7 +24,7 @@ def test_s3iterable_dataset_multiprocess():
         E2E_TEST_BUCKET,
         prefix=E2E_BUCKET_PREFIX,
         client=client,
-        transform=_verify_worker_info,
+        transform=_verify_object,
     )
 
     num_workers = 3
@@ -45,7 +51,7 @@ def test_s3iterable_dataset_multiprocess_skips_files():
         E2E_TEST_BUCKET,
         prefix=E2E_BUCKET_PREFIX,
         client=client,
-        transform=_verify_worker_info,
+        transform=_verify_object,
     )
 
     num_workers = 3
@@ -64,8 +70,18 @@ def test_s3iterable_dataset_multiprocess_skips_files():
     assert counter == num_epochs * num_images
 
 
-def _verify_worker_info(s3_object: S3Object) -> (int, int):
+def _verify_object(s3_object: S3Object) -> (int, int):
     assert s3_object._stream is None
+
+    image_key = image_key_re.search(s3_object.key).group(0)
+    local_file = os.path.join(ABSOLUTE_PATH, LOCAL_DATASET_RELATIVE_PATH, image_key)
+    with open(local_file, "rb") as local_image:
+        assert local_image.read() == s3_object.read()
+
+    return _get_worker_info()
+
+
+def _get_worker_info() -> (int, int):
     worker_info = get_worker_info()
     assert worker_info is not None
     return worker_info.id, worker_info.num_workers
