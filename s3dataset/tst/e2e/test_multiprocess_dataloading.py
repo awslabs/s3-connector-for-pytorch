5372d465d3c179b970474ca3800aae291a1d3fcd
Simon Beal <simobeal@amazon.com>
2023-11-13 11:05:47 +0000

    Improve tests for multiprocess dataloading

--- s3dataset/tst/e2e/test_multiprocess_dataloading.py
+++ s3dataset/tst/e2e/test_multiprocess_dataloading.py
@@ -1,5 +1,6 @@
+from collections import Counter
+
 from s3dataset_s3_client._s3dataset import MountpointS3Client
-from torch import multiprocessing
 from torch.utils.data import DataLoader, get_worker_info
 
 from s3dataset import S3IterableDataset
@@ -10,22 +11,8 @@ E2E_BUCKET_PREFIX = "e2e-tests/images-10/img"
 E2E_TEST_REGION = "eu-west-2"
 
 
-class Counter:
-    # A simple counter that's synced between processes.
-    def __init__(self):
-        self._value = multiprocessing.Value("i", 0)
-
-    def increment(self, n: int = 1, /):
-        with self._value.get_lock():
-            self._value.value += n
-
-    @property
-    def value(self):
-        return self._value.value
-
-
 def test_s3iterable_dataset_multiprocess():
-    counter = Counter()
+    counter = 0
     client = MountpointS3Client(E2E_TEST_REGION)
     dataset = S3IterableDataset.from_bucket(
         E2E_TEST_BUCKET,
@@ -40,14 +27,19 @@ def test_s3iterable_dataset_multiprocess():
 
     dataloader = DataLoader(dataset, num_workers=num_workers)
     for epoch in range(num_epochs):
-        for i, _ in enumerate(dataloader):
-            counter.increment()
-        assert (i + 1) == num_images * num_workers
-    assert counter.value == num_workers * num_epochs * num_images
+        worker_count = Counter()
+        for worker_id, _num_workers in dataloader:
+            counter += 1
+            worker_count[worker_id.item()] += 1
+            assert _num_workers == num_workers
+        assert len(worker_count) == num_workers
+        assert all(times_found == num_images for times_found in worker_count.values())
+        assert sum(worker_count.values()) == num_images * num_workers
+    assert counter == num_workers * num_epochs * num_images
 
 
 def test_s3iterable_dataset_multiprocess_skips_files():
-    counter = Counter()
+    counter = 0
     client = MountpointS3Client(E2E_TEST_REGION)
     dataset = S3IterableDataset.from_bucket(
         E2E_TEST_BUCKET,
@@ -64,13 +56,16 @@ def test_s3iterable_dataset_multiprocess_skips_files():
         dataset, num_workers=num_workers, worker_init_fn=dataset.worker_init
     )
     for epoch in range(num_epochs):
-        for i, _ in enumerate(dataloader):
-            counter.increment()
-    assert counter.value == num_epochs * num_images
+        worker_count = Counter()
+        for worker_id, _num_workers in dataloader:
+            counter += 1
+            worker_count[worker_id.item()] += 1
+        assert sum(worker_count.values()) == num_images
+    assert counter == num_epochs * num_images
 
 
-def _verify_worker_info(s3_object: S3Object) -> int:
+def _verify_worker_info(s3_object: S3Object) -> (int, int):
     assert s3_object._stream is None
     worker_info = get_worker_info()
     assert worker_info is not None
-    return 0
+    return worker_info.id, worker_info.num_workers
