282e59cd518ec7e9f4c51dc2a93c27f86fc20415
Simon Beal <simobeal@amazon.com>
2023-11-15 16:21:47 +0000

    Add multiprocess dataloading tests.
    All tests verify under all supported multiprocess spawn methods,
    as well as `from_prefix` and `from_objects`.
    
    `test_s3iterable_dataset_multiprocess_torchdata` verifies torchdata
    dataloader works as expected with multiprocessing.
    
    `test_s3iterable_dataset_multiprocess` verifies `S3IterableDataset`
    works as expected with multiprocessing
    `test_s3mapdataset_multiprocess` verifies `S3MapDataset`
    works as expected with multiprocessing.

---
+++ s3dataset/tst/e2e/test_multiprocess_dataloading.py
@@ -0,0 +1,175 @@
+from __future__ import annotations
+
+import platform
+from collections import Counter
+from itertools import product
+from typing import Tuple, Callable, TYPE_CHECKING
+
+import pytest
+import torch
+from torch.utils.data import DataLoader, get_worker_info
+from torchdata.datapipes.iter import IterableWrapper
+
+from s3dataset import S3IterableDataset, S3MapDataset
+from s3dataset_s3_client import S3Object
+
+if TYPE_CHECKING:
+    from .conftest import BucketPrefixFixture
+
+
+start_methods = set(torch.multiprocessing.get_all_start_methods())
+
+if platform.system() == "Darwin":
+    # fork and forkserver crash on MacOS, even though it's reported as usable.
+    # https://docs.python.org/3/library/multiprocessing.html#contexts-and-start-methods
+    # https://bugs.python.org/issue?@action=redirect&bpo=33725
+    start_methods -= {"fork", "forkserver"}
+
+
+def from_prefix(cls, image_directory: BucketPrefixFixture, **kwargs):
+    return cls.from_prefix(
+        s3_uri=f"s3://{image_directory.bucket}/{image_directory.prefix}",
+        region=image_directory.region,
+        **kwargs,
+    )
+
+
+def from_objects(cls, image_directory: BucketPrefixFixture, **kwargs):
+    return cls.from_objects(
+        [f"s3://{image_directory.bucket}/{key}" for key in image_directory],
+        region=image_directory.region,
+        **kwargs,
+    )
+
+
+# Allow us to construct our datasets in tests with either from_prefix or from_objects.
+dataset_builders = (from_prefix, from_objects)
+
+test_args = list(product(start_methods, dataset_builders))
+
+
+@pytest.mark.parametrize("start_method, dataset_builder", test_args)
+def test_s3iterable_dataset_multiprocess_torchdata(
+    start_method: str,
+    dataset_builder: Callable,
+    image_directory: BucketPrefixFixture,
+):
+    _set_start_method(start_method)
+    dataset = dataset_builder(S3IterableDataset, image_directory)
+
+    dataset = IterableWrapper(dataset, deepcopy=False).sharding_filter().map(_read_data)
+
+    batch_size = 2
+    num_workers = 3
+
+    dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=num_workers)
+
+    total_objects = 0
+    uris_seen = Counter()
+    for uris, datas in dataloader:
+        assert len(uris) == len(datas)
+        object_count = len(uris)
+        assert object_count <= batch_size
+        total_objects += object_count
+        for uri, data in zip(uris, datas):
+            assert isinstance(uri, str)
+            assert isinstance(data, bytes)
+            uris_seen[uri] += 1
+
+    # IterableWrapper has sharding enabled; we'll see each image once.
+    assert total_objects == len(image_directory.contents)
+    assert uris_seen == {key: 1 for key in image_directory}
+
+
+@pytest.mark.parametrize("start_method, dataset_builder", test_args)
+def test_s3iterable_dataset_multiprocess(
+    start_method: str,
+    dataset_builder: Callable,
+    image_directory: BucketPrefixFixture,
+):
+    _set_start_method(start_method)
+    dataset = dataset_builder(
+        S3IterableDataset,
+        image_directory,
+        transform=_extract_object_data,
+    )
+
+    num_workers = 3
+    num_epochs = 2
+    num_images = len(image_directory.contents)
+
+    dataloader = DataLoader(dataset, num_workers=num_workers)
+    counter = 0
+    for epoch in range(num_epochs):
+        s3keys = Counter()
+        worker_count = Counter()
+        for (s3key,), (contents,), worker_id, _num_workers in dataloader:
+            s3keys[s3key] += 1
+            counter += 1
+            worker_count[worker_id.item()] += 1
+            assert _num_workers == num_workers
+            assert image_directory[s3key] == contents
+        assert len(worker_count) == num_workers
+        assert all(times_found == num_images for times_found in worker_count.values())
+        # Iterable dataset does not do sharding; thus we'll see each image once for each worker.
+        assert sum(worker_count.values()) == num_images * num_workers
+        assert dict(s3keys) == {key: num_workers for key in image_directory}
+
+
+@pytest.mark.xfail
+@pytest.mark.parametrize("start_method, dataset_builder", test_args)
+def test_s3mapdataset_multiprocess(
+    start_method: str,
+    dataset_builder: Callable,
+    image_directory: BucketPrefixFixture,
+):
+    _set_start_method(start_method)
+    dataset = dataset_builder(
+        S3MapDataset,
+        image_directory,
+        transform=_extract_object_data,
+    )
+
+    num_workers = 3
+    num_epochs = 2
+    num_images = len(image_directory.contents)
+
+    dataloader = DataLoader(dataset, num_workers=num_workers)
+
+    for epoch in range(num_epochs):
+        s3keys = Counter()
+        worker_count = Counter()
+        if start_method == "fork":
+            raise NotImplementedError(
+                "This does not work, and hangs rather than failing."
+            )
+        # FIXME - torch dataloader.__iter__ calls S3MapDataset.__len__ internally before child processes spawn.
+        # This triggers the client to be created in the main process, keeping itself around when child processes spawn.
+        # Our client, MountpointS3Client, explodes when it's used in a child process with `fork` spawn type.
+        for (s3key,), (contents,), worker_id, _num_workers in dataloader:
+            worker_count[worker_id.item()] += 1
+            s3keys[s3key] += 1
+            assert _num_workers == num_workers
+            assert image_directory[s3key] == contents
+        # Map dataset does sharding; we'll see each image once.
+        assert sum(worker_count.values()) == num_images
+        assert dict(s3keys) == {key: 1 for key in image_directory}
+    # TODO - assert len(dataloader) == num_images
+
+
+def _set_start_method(start_method: str):
+    torch.multiprocessing.set_start_method(start_method, force=True)
+
+
+def _extract_object_data(s3_object: S3Object) -> (str, bytes, int, int):
+    return s3_object.key, s3_object.read(), *_get_worker_info()
+
+
+def _get_worker_info() -> (int, int):
+    worker_info = get_worker_info()
+    assert worker_info is not None
+    return worker_info.id, worker_info.num_workers
+
+
+def _read_data(s3_object: S3Object) -> Tuple[str, bytes]:
+    return s3_object.key, s3_object.read()
