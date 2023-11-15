e3cc10daf8d1a9755cc87bd4e00b7b779b937d73
James Bornholt <bornholt@amazon.com>
2023-11-15 10:08:45 -0600

    Add a basic integration test workflow (#34)
    * Add a basic integration test workflow
    
    Signed-off-by: James Bornholt <bornholt@amazon.com>
    
    * Fix naming and comments
    
    Signed-off-by: James Bornholt <bornholt@amazon.com>
    
    * Only install CPU version of PyTorch
    
    Signed-off-by: James Bornholt <bornholt@amazon.com>
    
    * Now I remember why we don't do this
    
    Signed-off-by: James Bornholt <bornholt@amazon.com>
    
    * Use region for boto3 client
    
    Signed-off-by: James Bornholt <bornholt@amazon.com>
    
    ---------
    
    Signed-off-by: James Bornholt <bornholt@amazon.com>

---
+++ s3dataset/tst/e2e/conftest.py
@@ -0,0 +1,80 @@
+from dataclasses import dataclass
+import io
+import os
+import random
+from typing import Dict
+
+import boto3
+import numpy as np
+from PIL import Image
+import pytest
+
+
+def getenv(var: str) -> str:
+    v = os.getenv(var)
+    if v is None:
+        raise Exception(f"required environment variable {var} is not set")
+    return v
+
+
+class BucketPrefixFixture(object):
+    """An S3 bucket/prefix and its contents for use in a single unit test. The prefix will be unique
+    to this instance, so other concurrent tests won't affect its state."""
+
+    region: str
+    bucket: str
+    prefix: str
+
+    def __init__(self, region: str, bucket: str, prefix: str):
+        self.bucket = bucket
+        self.prefix = prefix
+        self.region = region
+        self.contents = {}
+        session = boto3.Session(region_name=region)
+        self.s3 = session.client("s3")
+
+    def add(self, key: str, contents: bytes, **kwargs):
+        """Upload an S3 object to this prefix of the bucket."""
+        full_key = f"{self.prefix}{key}"
+        self.s3.put_object(Bucket=self.bucket, Key=full_key, Body=contents, **kwargs)
+        self.contents[full_key] = contents
+
+    def __getitem__(self, index):
+        return self.contents[index]
+
+    def __iter__(self):
+        return iter(self.contents)
+
+
+def get_test_bucket_prefix(name: str) -> BucketPrefixFixture:
+    """Create a new bucket/prefix fixture for the given test name."""
+    bucket = getenv("CI_BUCKET")
+    prefix = getenv("CI_PREFIX")
+    region = getenv("CI_REGION")
+    assert prefix == "" or prefix.endswith("/")
+
+    nonce = random.randrange(2**64)
+    prefix = f"{prefix}{name}/{nonce}/"
+
+    return BucketPrefixFixture(region, bucket, prefix)
+
+
+@pytest.fixture
+def image_directory(request) -> BucketPrefixFixture:
+    """Create a bucket/prefix fixture that contains a directory of random JPG image files."""
+    NUM_IMAGES = 10
+    IMAGE_SIZE = 100
+    fixture = get_test_bucket_prefix(f"{request.node.name}/image_directory")
+    for i in range(NUM_IMAGES):
+        data = np.random.randint(0, 256, IMAGE_SIZE * IMAGE_SIZE * 3, np.uint8)
+        data = data.reshape(IMAGE_SIZE, IMAGE_SIZE, 3)
+        image = Image.fromarray(data, "RGB")
+        image_bytes = io.BytesIO()
+        image.save(image_bytes, "jpeg")
+        image_bytes.seek(0)
+        image_bytes = image_bytes.read()
+
+        key = f"img{i:03d}.jpg"
+        fixture.add(key, image_bytes)
+
+    return fixture
