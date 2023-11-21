dfb6119089fd3ac2593c781b80d3a83503d26670
dnnanuti <dnnanuti@amazon.com>
2023-11-21 13:40:19 +0000

    Remove s3dataset_base.py and refactor S3 datasets
    We do not want to expose S3DatasetBase and we face challenges
    generating documentation while extending this class.

--- s3torchconnector/src/s3torchconnector/__init__.py
+++ s3torchconnector/src/s3torchconnector/__init__.py
@@ -4,7 +4,12 @@ from s3torchconnectorclient import S3Exception
 
 from .s3checkpoint import S3Checkpoint
 from ._s3client import S3Reader, S3Writer
-from .s3dataset_base import S3DatasetBase
+from ._s3dataset_common import (
+    parse_s3_uri,
+    get_objects_from_uris,
+    bucket_key_pairs_to_objects,
+    list_objects_from_prefix,
+)
 from .s3iterable_dataset import S3IterableDataset
 from .s3map_dataset import S3MapDataset
 
