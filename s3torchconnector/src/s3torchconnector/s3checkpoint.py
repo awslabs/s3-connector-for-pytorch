610cb6704154d8ef069a00186ed32f2eb731bf13
fuatbasik <fuatbasik@gmail.com>
2023-11-22 14:31:24 +0000

    Introduce Sphinx and github workflows for apidocs auto-generation. (#63)
    * Introduce Sphinx and github workflows for apidocs auto-generation.
    
    This commit adds Sphinx support for generating documentation from code,
    add adds necessary github actions to import generated document into
    github. Documentation is kept on a new branch `gh-pages` and overridden
    with every merge. We use this branch to serve our github io pages that
    could be accessed by visiting  awslabs.github.io/s3-connector-for-pytorch
    
    * Introduce Sphinx and github workflows for apidocs auto-generation.
    
    This commit adds Sphinx support for generating documentation from code,
    add adds necessary github actions to import generated document into
    github. Documentation is kept on a new branch `gh-pages` and overridden
    with every merge. We use this branch to serve our github io pages that
    could be accessed by visiting  awslabs.github.io/s3-connector-for-pytorch

--- s3torchconnector/src/s3torchconnector/s3checkpoint.py
+++ s3torchconnector/src/s3torchconnector/s3checkpoint.py
@@ -6,14 +6,45 @@ from s3torchconnector._s3client import S3Client, S3Reader, S3Writer
 
 
 class S3Checkpoint:
+    """A checkpoint manager for S3.
+
+    To read a checkpoint from S3, users need to create an S3Reader
+    by providing s3_uri of the checkpoint stored in S3. Similarly, to save a
+    checkpoint to S3, users need to create an S3Writer by providing s3_uri.
+    S3Reader and S3Writer implements io.BufferedIOBase therefore, they can be passed to
+    torch.load, and torch.save.
+    """
+
     def __init__(self, region: str):
         self.region = region
         self._client = S3Client(region)
 
     def reader(self, s3_uri: str) -> S3Reader:
+        """Creates an S3Reader from a given s3_uri.
+
+        Args:
+            s3_uri (str): A valid s3_uri. (i.e. s3://<BUCKET>/<KEY>)
+
+        Returns:
+            S3Reader: a read-only binary stream of the S3 object's contents, specified by the s3_uri.
+
+        Raises:
+            S3Exception: An error occurred accessing S3.
+        """
         bucket, key = parse_s3_uri(s3_uri)
         return self._client.get_object(bucket, key)
 
     def writer(self, s3_uri: str) -> S3Writer:
+        """Creates an S3Writer from a given s3_uri.
+
+        Args:
+            s3_uri (str): A valid s3_uri. (i.e. s3://<BUCKET>/<KEY>)
+
+        Returns:
+            S3Writer: a write-only binary stream. The content is saved to S3 using the specified s3_uri.
+
+        Raises:
+            S3Exception: An error occurred accessing S3.
+        """
         bucket, key = parse_s3_uri(s3_uri)
         return self._client.put_object(bucket, key)
