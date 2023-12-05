64b8cfe0692be5f9fb4f2fe23fac16a61304e78a
Simon Beal <simobeal@amazon.com>
2023-12-05 14:58:47 +0000

    Make timeout adjustable

--- conftest.py
+++ conftest.py
@@ -10,8 +10,10 @@ from hypothesis import settings
 settings.register_profile("ci", max_examples=1000, deadline=timedelta(seconds=1))
 settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "default"))
 
+per_test_timeout = int(os.getenv("TEST_TIMEOUT", "120"))
+
 
 def pytest_collection_modifyitems(items):
     for item in items:
         if item.get_closest_marker("timeout") is None:
-            item.add_marker(pytest.mark.timeout(120))
+            item.add_marker(pytest.mark.timeout(per_test_timeout))
