8589cc41c2af7db3eacb2070feb958dac8a5c8bb
Simon Beal <simobeal@amazon.com>
2023-12-05 14:43:12 +0000

    Add a 2-minute timeout to tests

--- conftest.py
+++ conftest.py
@@ -4,7 +4,14 @@
 import os
 from datetime import timedelta
 
+import pytest
 from hypothesis import settings
 
 settings.register_profile("ci", max_examples=1000, deadline=timedelta(seconds=1))
 settings.load_profile(os.getenv("HYPOTHESIS_PROFILE", "default"))
+
+
+def pytest_collection_modifyitems(items):
+    for item in items:
+        if item.get_closest_marker("timeout") is None:
+            item.add_marker(pytest.mark.timeout(120))
