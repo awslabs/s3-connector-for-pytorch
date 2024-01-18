020d19a8538d332fc94a6ad914d133b19d4963c2
Diana Nanuti <diana.nanuti@gmail.com>
2024-01-18 17:17:16 +0000

    Expose function to enable debug logging (#109)
    Experimental function to enable CRT logs for debugging purposes.
    Please do NOT use this method unless otherwise instructed.

--- s3torchconnectorclient/python/src/s3torchconnectorclient/_logger_patch.py
+++ s3torchconnectorclient/python/src/s3torchconnectorclient/_logger_patch.py
@@ -2,9 +2,19 @@
 #  // SPDX-License-Identifier: BSD
 
 import logging
+import atexit
+
+from ._mountpoint_s3_client import _enable_crt_logging, _disable_logging
 
 TRACE = 5
 
 
 def _install_trace_logging():
     logging.addLevelName(TRACE, "TRACE")
+
+
+# Experimental method for enabling verbose logging.
+# Please do NOT use unless otherwise instructed.
+def _enable_debug_logging():
+    atexit.register(_disable_logging)
+    _enable_crt_logging()
--- s3torchconnectorclient/rust/src/lib.rs
+++ s3torchconnectorclient/rust/src/lib.rs
@@ -4,6 +4,7 @@
  */
 
 use log::LevelFilter;
+use mountpoint_s3_crt::common::rust_log_adapter::RustLogAdapter;
 use pyo3::prelude::*;
 use pyo3_log::Logger;
 
@@ -27,6 +28,24 @@ mod put_object_stream;
 mod python_structs;
 mod build_info;
 
+
+/// Experimental method to enable CRT logs for debugging purposes.
+/// Please do NOT call this method unless otherwise instructed.
+#[pyfunction]
+#[pyo3(name = "_enable_crt_logging")]
+fn _enable_crt_logging() {
+    let _ = RustLogAdapter::try_init().map_err(python_exception);
+}
+
+
+/// Experimental method to prevent deadlocks when enabling verbose CRT logging:
+/// https://docs.rs/pyo3-log/latest/pyo3_log/#interaction-with-python-gil
+#[pyfunction]
+#[pyo3(name = "_disable_logging")]
+fn _disable_logging() {
+    Logger::default().reset_handle();
+}
+
 #[pymodule]
 #[pyo3(name = "_mountpoint_s3_client")]
 fn make_lib(py: Python, mountpoint_s3_client: &PyModule) -> PyResult<()> {
@@ -49,5 +68,7 @@ fn make_lib(py: Python, mountpoint_s3_client: &PyModule) -> PyResult<()> {
     mountpoint_s3_client.add_class::<PyRestoreStatus>()?;
     mountpoint_s3_client.add("S3Exception", py.get_type::<S3Exception>())?;
     mountpoint_s3_client.add("__version__", build_info::FULL_VERSION)?;
+    mountpoint_s3_client.add_function(wrap_pyfunction!(_enable_crt_logging, mountpoint_s3_client)?)?;
+    mountpoint_s3_client.add_function(wrap_pyfunction!(_disable_logging, mountpoint_s3_client)?)?;
     Ok(())
 }
