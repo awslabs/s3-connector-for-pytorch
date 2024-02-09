58d176ae6dccf9073a8b4b90c21c5f3be2acd375
Simon Beal <5381483+muddyfish@users.noreply.github.com>
2024-02-09 15:04:29 +0000

    Lightning support (#132)
    - Add testing which splits out utils for checkpointing into a common module
    - Add lightning optional dependency list for lightning checkpoints
    - Add delete tests
    - Add tests that validate the error message we get if lightning is not installed
    
    ---------
    
    Co-authored-by: dnnanuti <dnnanuti@amazon.com>
    Co-authored-by: Simon Beal <simobeal@amazon.com>

---
+++ s3torchconnector/tst/unit/_checkpoint_byteorder_patch.py
@@ -0,0 +1,26 @@
+#  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#  // SPDX-License-Identifier: BSD
+
+from contextlib import contextmanager
+from unittest.mock import patch
+import torch
+from hypothesis.strategies import one_of, just
+
+byteorders = one_of(just("little"), just("big"))
+
+
+@contextmanager
+def _patch_byteorder(byteorder: str):
+    with patch("torch.serialization.sys") as mock_sys:
+        mock_sys.byteorder = byteorder
+        yield
+
+
+def save_with_byteorder(data, fobj, byteorder: str, use_modern_pytorch_format: bool):
+    with _patch_byteorder(byteorder):
+        torch.save(data, fobj, _use_new_zipfile_serialization=use_modern_pytorch_format)
+
+
+def load_with_byteorder(fobj, byteorder):
+    with _patch_byteorder(byteorder):
+        return torch.load(fobj)
