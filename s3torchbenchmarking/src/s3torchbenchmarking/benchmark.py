bf89720a4ed54b418b6296374b2a17f8e8ee346c
Vishal Gowda <vgd@amazon.com>
2024-02-09 12:45:30 +0000

    Support benchmarking mountpoint-backed datasets

--- s3torchbenchmarking/src/s3torchbenchmarking/benchmark.py
+++ s3torchbenchmarking/src/s3torchbenchmarking/benchmark.py
@@ -1,19 +1,23 @@
 #  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #  // SPDX-License-Identifier: BSD
-
-
+import atexit
+import shutil
+import subprocess
+import tempfile
 from enum import Enum
-from typing import Optional
+from pathlib import Path
+from typing import Optional, List
 
 import hydra
 import torchdata
 from omegaconf import DictConfig
-from s3torchconnector import S3IterableDataset, S3Reader, S3MapDataset
 from torch.utils.data import DataLoader, Dataset, default_collate
 from torchdata.datapipes.utils import StreamWrapper
 
 from s3torchbenchmarking.benchmark_utils import ResourceMonitor
 from s3torchbenchmarking.models import Entitlement, ViT, ModelInterface
+from s3torchconnector import S3IterableDataset, S3Reader, S3MapDataset
+from s3torchconnector._s3dataset_common import parse_s3_uri
 
 
 @hydra.main(version_base=None)
@@ -71,6 +75,27 @@ class DatasetSharding(Enum):
             return DatasetSharding[dataset_config.sharding]
 
 
+def make_mountpoint(
+    prefix_uri: str,
+    mountpoint_path: Optional[str] = None,
+    additional_args: List[str] = None,
+) -> str:
+    def teardown(path: str):
+        subprocess.run(["sudo", "umount", path])
+        shutil.rmtree(path)
+
+    bucket, prefix = parse_s3_uri(prefix_uri)
+    # Run Mountpoint in background mode, and arrange for it to unmount when this script exits
+    tempdir = tempfile.mkdtemp(prefix="s3dataset_")
+    binary = mountpoint_path or "mount-s3"
+    args = additional_args or []
+    subprocess.run([binary, bucket, tempdir] + args, check=True)
+    atexit.register(teardown, tempdir)
+
+    # Now we can just read our dataset as if it were a local directory
+    return str(Path(tempdir) / prefix)
+
+
 def make_dataset(
     kind: str,
     sharding: Optional[DatasetSharding],
@@ -80,32 +105,71 @@ def make_dataset(
     num_workers: int,
 ):
     if kind == "s3iterabledataset":
-        dataset = S3IterableDataset.from_prefix(prefix_uri, region=region)
-        dataset = torchdata.datapipes.iter.IterableWrapper(dataset)
-        if num_workers > 0:
-            dataset = dataset.sharding_filter()
-        if sharding == DatasetSharding.TAR:
-            dataset = dataset.map(tar_to_tuple)
-            dataset = dataset.load_from_tar()
-        return dataset.map(load_sample)
+        return create_s3_iterable_dataset(
+            sharding, prefix_uri, region, load_sample, num_workers
+        )
     elif kind == "s3mapdataset":
-        if sharding:
-            raise ValueError(f"Sharding is not supported for {kind}")
-        else:
-            dataset = S3MapDataset.from_prefix(
-                prefix_uri, region=region, transform=load_sample
-            )
-            return dataset
+        return create_s3_map_dataset(sharding, prefix_uri, region, load_sample)
     elif kind == "fsspec":
-        lister = torchdata.datapipes.iter.FSSpecFileLister(prefix_uri)
-        dataset = torchdata.datapipes.iter.FSSpecFileOpener(lister, mode="rb")
-        if num_workers > 0:
-            dataset = dataset.sharding_filter()
-        if sharding == DatasetSharding.TAR:
-            dataset = dataset.load_from_tar()
-        return dataset.map(load_sample)
+        return create_fsspec_dataset(sharding, prefix_uri, load_sample, num_workers)
+    elif kind == "mountpoint":
+        return create_mountpoint_dataset(sharding, prefix_uri, load_sample, num_workers)
     else:
         raise Exception(f"unknown dataset kind {kind}")
+
+
+def create_s3_iterable_dataset(
+    sharding: Optional[DatasetSharding],
+    prefix_uri: str,
+    region: str,
+    load_sample,
+    num_workers: int,
+):
+    dataset = S3IterableDataset.from_prefix(prefix_uri, region=region)
+    dataset = torchdata.datapipes.iter.IterableWrapper(dataset)
+    if num_workers > 0:
+        dataset = dataset.sharding_filter()
+    if sharding == DatasetSharding.TAR:
+        dataset = dataset.map(tar_to_tuple)
+        dataset = dataset.load_from_tar()
+
+    return dataset.map(load_sample)
+
+
+def create_s3_map_dataset(
+    sharding: Optional[DatasetSharding], prefix_uri: str, region: str, load_sample
+):
+    if sharding:
+        raise ValueError("Sharding is not supported for s3mapdataset")
+    else:
+        dataset = S3MapDataset.from_prefix(
+            prefix_uri, region=region, transform=load_sample
+        )
+    return dataset
+
+
+def create_mountpoint_dataset(
+    sharding: Optional[DatasetSharding], prefix_uri: str, load_sample, num_workers: int
+):
+    prefix_uri = make_mountpoint(prefix_uri=prefix_uri)
+    # TODO: compare the performance of using torchdata file APIs and use the more performant option.
+    return create_fsspec_dataset(sharding, prefix_uri, load_sample, num_workers)
+
+
+def create_fsspec_dataset(
+    sharding: Optional[DatasetSharding],
+    prefix_uri: str,
+    load_sample: object,
+    num_workers: int,
+):
+    lister = torchdata.datapipes.iter.FSSpecFileLister(prefix_uri)
+    dataset = torchdata.datapipes.iter.FSSpecFileOpener(lister, mode="rb")
+    if num_workers > 0:
+        dataset = dataset.sharding_filter()
+    if sharding == DatasetSharding.TAR:
+        dataset = dataset.load_from_tar()
+
+    return dataset.map(load_sample)
 
 
 def make_dataloader(dataset: Dataset, num_workers: int, batch_size: int):
