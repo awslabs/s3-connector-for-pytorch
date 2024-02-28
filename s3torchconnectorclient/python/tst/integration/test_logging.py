5f9deb3b1c75a6c288b2eb69a043ce6f94eb38df
dnnanuti <dnnanuti@amazon.com>
2024-02-28 16:33:47 +0000

    Update logging setup to log to file if CRT_LOGS_DIR_PATH environment variable is set with a logging directory path.

--- s3torchconnectorclient/python/tst/integration/test_logging.py
+++ s3torchconnectorclient/python/tst/integration/test_logging.py
@@ -1,6 +1,9 @@
 #  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #  // SPDX-License-Identifier: BSD
+import os.path
 import sys
+from datetime import datetime
+
 import pytest
 
 PYTHON_TEST_CODE = """
@@ -9,6 +12,10 @@ import os
 import sys
 
 os.environ["ENABLE_CRT_LOGS"] = "{0}"
+
+logs_dir_path = "{1}"
+if logs_dir_path != "":
+    os.environ["CRT_LOGS_DIR_PATH"] = logs_dir_path
 
 from s3torchconnector import S3MapDataset
 
@@ -39,20 +46,23 @@ import subprocess
             ["DEBUG", "TRACE"],
         ),
         (
-            "debug",
+            "DEBUG",
             [
                 "INFO s3torchconnector.s3map_dataset",
                 "DEBUG awscrt::AWSProfile",
                 "DEBUG awscrt::AuthCredentialsProvider",
+                "DEBUG awscrt::S3Client",
             ],
             ["TRACE"],
         ),
         (
-            "trace",
+            "TRACE",
             [
                 "INFO s3torchconnector.s3map_dataset",
                 "DEBUG awscrt::AWSProfile",
                 "DEBUG awscrt::AuthCredentialsProvider",
+                "TRACE awscrt::event-loop",
+                "TRACE awscrt::socket",
                 "TRACE awscrt::event-loop",
             ],
             # Python log level is set to INFO in the test script
@@ -61,36 +71,51 @@ import subprocess
     ],
 )
 def test_logging_valid(log_level, should_contain, should_not_contain, image_directory):
-    stdout, stderr = _start_subprocess(log_level, image_directory)
-    assert stderr == ""
-    assert stdout != ""
-    assert all(s in stdout for s in should_contain)
-    assert all(s not in stdout for s in should_not_contain)
+    out, err = _start_subprocess(log_level, image_directory)
+    assert err == ""
+    assert all(s in out for s in should_contain)
+    assert all(s not in out for s in should_not_contain)
+
+
+def test_logging_to_file(image_directory):
+    dt_string = datetime.now().strftime("%d-%m-%Y-%H-%M-%S.%f")
+    log_dir = f"/tmp/test-logs-{dt_string}/"
+    out, err = _start_subprocess("INFO", image_directory, log_dir)
+    # Standard output contains Python output
+    assert err == ""
+    assert "INFO s3torchconnector.s3map_dataset" in out
+    assert "awscrt" not in out
+    # Verifying file logging
+    assert os.path.exists(log_dir)
+    files = os.listdir(log_dir)
+    assert len(files) == 1
+    log_file = f"{log_dir}{files[0]}"
+    assert os.path.isfile(log_file)
+    f = open(log_file, "r")
+    file_content = f.read()
+    assert all(s in file_content for s in ["awscrt", "INFO"])
+    assert all(
+        s not in file_content
+        for s in ["INFO s3torchconnector.s3map_dataset", "DEBUG", "TRACE"]
+    )
+    # Cleanup logs file and directory
+    os.remove(log_file)
+    os.rmdir(log_dir)
 
 
 def test_logging_off(image_directory):
-    stdout, stderr = _start_subprocess("off", image_directory)
-    assert stderr == ""
-    assert stdout != ""
-    assert "INFO s3torchconnector.s3map_dataset" in stdout
-    assert "awscrt" not in stdout
+    out, err = _start_subprocess("off", image_directory)
+    assert err == ""
+    assert "INFO s3torchconnector.s3map_dataset" in out
+    assert "awscrt" not in out
 
 
-def test_logging_invalid(image_directory):
-    stdout, stderr = _start_subprocess("123", image_directory)
-    assert stdout == ""
-    assert (
-        "s3torchconnectorclient._mountpoint_s3_client.S3Exception: attempted to convert a string that doesn't match an existing log level"
-        in stderr
-    )
-
-
-def _start_subprocess(log_level, image_directory):
+def _start_subprocess(log_level, image_directory, logs_directory=""):
     process = subprocess.Popen(
         [
             sys.executable,
             "-c",
-            PYTHON_TEST_CODE.format(log_level),
+            PYTHON_TEST_CODE.format(log_level, logs_directory),
             image_directory.s3_uri,
             image_directory.region,
         ],
--- s3torchconnectorclient/rust/src/logger_setup.rs
+++ s3torchconnectorclient/rust/src/logger_setup.rs
@@ -2,55 +2,51 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * // SPDX-License-Identifier: BSD
  */
-use std::{env, io::Write};
-use std::str::FromStr;
-use env_logger::{Builder, Target};
+use std::{env};
 use log::{LevelFilter};
 use mountpoint_s3_crt::common::rust_log_adapter::RustLogAdapter;
 use pyo3::{PyResult};
-use chrono::Local;
 use pyo3_log::Logger;
+use tracing_subscriber::{filter::EnvFilter};
+use tracing_subscriber::fmt::writer::{MakeWriterExt};
+use tracing_subscriber::util::{SubscriberInitExt};
 use crate::exception::python_exception;
 
 pub const ENABLE_CRT_LOGS_ENV_VAR: &str = "ENABLE_CRT_LOGS";
-const DATE_FORMAT_STR: &str = "%Y-%m-%d %H:%M:%S%.3f";
+pub const CRT_LOGS_DIR_PATH_ENV_VAR: &str = "CRT_LOGS_DIR_PATH";
 
 pub fn setup_logging() -> PyResult<()> {
-    let enable_crt_logs = env::var(ENABLE_CRT_LOGS_ENV_VAR)
-        .unwrap_or(String::from("OFF"))
-        .to_uppercase();
+    let enable_crt_logs = env::var(ENABLE_CRT_LOGS_ENV_VAR).ok();
 
-    match enable_crt_logs.as_str() {
-        "OFF" => enable_default_logging(),
-        level_filter_str => enable_crt_logging(level_filter_str)
+    match enable_crt_logs {
+        None => enable_default_logging(),
+        Some(..) => enable_crt_logging()
     }
 }
 
-fn enable_crt_logging(level_filter_str: &str) -> PyResult<()> {
-    let level_filter = LevelFilter::from_str(level_filter_str)
-        .map_err(python_exception)?;
-
+fn enable_crt_logging() -> PyResult<()> {
     RustLogAdapter::try_init().map_err(python_exception)?;
 
-    let mut builder = Builder::new();
-    builder
-        .format(|buf, record| {
-            writeln!(buf, "{} {} {} {} {} {}",
-                     record.level(), record.target(),
-                     Local::now().format(DATE_FORMAT_STR),
-                     record.module_path().unwrap(),
-                     record.line().unwrap(),
-                     record.args())
-        })
-        .target(Target::Stdout)
-        .format_module_path(false)
-        .filter_module(
-            "mountpoint_s3_client::s3_crt_client",
-            LevelFilter::Off,
-        )
-        .filter_level(level_filter);
+    let filter = EnvFilter::try_from_env(ENABLE_CRT_LOGS_ENV_VAR).map_err(python_exception);
+    let crt_logs_path = env::var(CRT_LOGS_DIR_PATH_ENV_VAR).ok();
+    match crt_logs_path {
+        Some(logs_path) => {
+            let logfile = tracing_appender::rolling::hourly(logs_path, "s3torchconnectorclient.log");
+            let subscriber_builder = tracing_subscriber::fmt()
+                .with_writer(logfile)
+                 .with_env_filter(filter.unwrap_or_default())
+                 .with_ansi(false);
+            subscriber_builder.finish().try_init().map_err(python_exception)?;
+        },
+        None => {
+            let subscriber_builder = tracing_subscriber::fmt()
+                 .with_env_filter(filter.unwrap_or_default())
+                 .with_ansi(false);
+            subscriber_builder.finish().try_init().map_err(python_exception)?;
+        }
+    }
 
-    builder.try_init().map_err(python_exception)
+    Ok(())
 }
 
 fn enable_default_logging() -> PyResult<()> {
@@ -72,6 +68,13 @@ mod tests {
     use std::{env};
     use pyo3::PyResult;
     use crate::logger_setup::{ENABLE_CRT_LOGS_ENV_VAR, setup_logging};
+
+    fn check_valid_log_level(log_level: &str) {
+        pyo3::prepare_freethreaded_python();
+        env::set_var(ENABLE_CRT_LOGS_ENV_VAR, log_level);
+        let result: PyResult<()> = setup_logging();
+        assert!(result.is_ok());
+    }
 
     rusty_fork_test! {
         #[test]
@@ -111,25 +114,5 @@ mod tests {
         fn test_logging_level_trace() {
             check_valid_log_level("trace");
         }
-
-        #[test]
-        fn test_invalid_values() {
-            pyo3::prepare_freethreaded_python();
-            let invalid_values = ["invalid", "", "\n", "123", "xyz"];
-            for value in invalid_values.iter() {
-                env::set_var(ENABLE_CRT_LOGS_ENV_VAR, *value);
-                let error_result: PyResult<()> = setup_logging();
-                assert!(error_result.is_err());
-                let pyerr = error_result.err().unwrap();
-                assert_eq!(pyerr.to_string(), "S3Exception: attempted to convert a string that doesn't match an existing log level");
-            }
-        }
-    }
-
-    fn check_valid_log_level(log_level: &str) {
-        pyo3::prepare_freethreaded_python();
-        env::set_var(ENABLE_CRT_LOGS_ENV_VAR, log_level);
-        let result: PyResult<()> = setup_logging();
-        assert!(result.is_ok());
     }
 }
