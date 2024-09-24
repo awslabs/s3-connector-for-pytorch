da7db19cf1cf1104b223521314afc1236ba1cf93
rajdchak <rajdchak@amazon.co.uk>
2024-09-24 17:23:23 +0100

    Added integration test to test parallel saving of checkpoints

--- s3torchconnector/tst/e2e/test_e2e_s3checkpoint.py
+++ s3torchconnector/tst/e2e/test_e2e_s3checkpoint.py
@@ -6,7 +6,7 @@ import pytest
 
 from s3torchconnector import S3Checkpoint
 from models.net import Net
-
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 @pytest.mark.parametrize(
     "tensor_dimensions",
@@ -17,8 +17,7 @@ def test_general_checkpointing(checkpoint_directory, tensor_dimensions):
     checkpoint_name = "general_checkpoint.pt"
     checkpoint = S3Checkpoint(region=checkpoint_directory.region)
     s3_uri = f"{checkpoint_directory.s3_uri}/{checkpoint_name}"
-    with checkpoint.writer(s3_uri) as writer:
-        torch.save(tensor, writer)
+    save_checkpoint(s3_uri, checkpoint, tensor)
 
     loaded = torch.load(checkpoint.reader(s3_uri))
 
@@ -34,15 +33,11 @@ def test_nn_checkpointing(checkpoint_directory):
     s3_uri = f"{checkpoint_directory.s3_uri}/{checkpoint_name}"
     loss = 0.4
 
-    with checkpoint.writer(s3_uri) as writer:
-        torch.save(
-            {
+    save_checkpoint(s3_uri, checkpoint, {
                 "epoch": epoch,
                 "model_state_dict": nn_model.state_dict(),
                 "loss": loss,
-            },
-            writer,
-        )
+            })
 
     loaded_nn_model = Net()
 
@@ -61,3 +56,32 @@ def test_nn_checkpointing(checkpoint_directory):
     # Assert that eval and train do not raise
     loaded_nn_model.eval()
     loaded_nn_model.train()
+
+def test_parallel_checkpointing(checkpoint_directory):
+    checkpoint = S3Checkpoint(region=checkpoint_directory.region)
+    tensor = torch.rand(2,3)
+    results = []
+    futures = []
+
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        for i in range(100):
+            checkpoint_name = f"general_checkpoint.pt_{i}"
+            s3_uri = f"{checkpoint_directory.s3_uri}/{checkpoint_name}"
+            futures.append(executor.submit(save_checkpoint, s3_uri, checkpoint, tensor))
+
+        for future in as_completed(futures):
+            try:
+                result = future.result()  # Get the result of the completed future
+                results.append(result)
+            except AssertionError as e:
+                results.append(f"AssertionError: {e}")
+            except Exception as e:
+                results.append(f"Error: {e}")
+
+    assert not any("AssertionError" in str(res) for res in results), "An AssertionError was encountered."
+
+def save_checkpoint(s3_uri, checkpoint: S3Checkpoint, checkpoint_content):
+    with checkpoint.writer(s3_uri) as writer:
+        torch.save(checkpoint_content, writer)
+
+
