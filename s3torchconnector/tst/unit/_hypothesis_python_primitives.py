2fb1b51817baad318a4712ef8b72d5ada1b7b5c9
Ilya Isaev <iisaev@amazon.com>
2025-02-04 14:44:36 +0000

    Align test types with torch.load weights_only support

--- s3torchconnector/tst/unit/_hypothesis_python_primitives.py
+++ s3torchconnector/tst/unit/_hypothesis_python_primitives.py
@@ -3,47 +3,32 @@
 
 from hypothesis.strategies import (
     integers,
-    binary,
-    none,
     characters,
-    complex_numbers,
     floats,
     booleans,
-    decimals,
-    fractions,
     deferred,
-    frozensets,
     tuples,
     dictionaries,
     lists,
-    uuids,
-    sets,
     text,
 )
 
 scalars = (
-    none()
-    | booleans()
+    booleans()
     | integers()
     # Disallow nan as it doesn't have self-equality
     | floats(allow_nan=False)
-    | complex_numbers(allow_nan=False)
-    | decimals(allow_nan=False)
-    | fractions()
     | characters()
-    | binary(max_size=10)
     | text(max_size=10)
-    | uuids()
 )
 
 hashable = deferred(
-    lambda: (scalars | frozensets(hashable, max_size=5) | tuples(hashable))
+    lambda: (scalars | tuples(hashable))
 )
 
 python_primitives = deferred(
     lambda: (
         hashable
-        | sets(hashable, max_size=5)
         | lists(python_primitives, max_size=5)
         | dictionaries(keys=hashable, values=python_primitives, max_size=3)
     )
