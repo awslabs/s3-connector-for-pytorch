23206f68074d9fcfc593d84ab599f4980d0db625
Ilya Isaev <iisaev@amazon.com>
2025-05-02 15:05:32 +0100

    Add e2e test for Binary strategy

--- s3torchconnector/src/s3torchconnector/dcp/s3_prefix_strategy.py
+++ s3torchconnector/src/s3torchconnector/dcp/s3_prefix_strategy.py
@@ -4,6 +4,7 @@ from abc import ABC, abstractmethod
 from typing import List
 
 import torch.distributed as dist
+from sympy import variations
 
 
 class S3PrefixStrategyBase(ABC):
@@ -33,19 +34,42 @@ class DefaultPrefixStrategy(S3PrefixStrategyBase):
 class NumericPrefixStrategy(S3PrefixStrategyBase):
     """Base class for numeric prefix generation strategies."""
 
-    def __init__(self, base: int, epoch_num: int = None, min_prefix_len: int = 10):
+    def __init__(self, base: int, epoch_num: int = None, min_prefix_length: int = 10, prefix_count: int = None):
         """
-        Initialize numeric prefix strategy.
+            Initialize numeric prefix strategy.
 
         Args:
-            base: The numeric base for the prefix (e.g., 2 for binary, 16 for hex).
-            epoch_num: Epoch number for checkpoint ordering. If None,
-                       no epoch information will be included in the prefix.
+            base (int): The numeric base for the prefix (e.g., 2 for binary, 16 for hex).
+            epoch_num (int, optional): Epoch number for checkpoint ordering. If None,
+                no epoch information will be included in the prefix. Defaults to None.
+            min_prefix_length (int): Minimum length of the generated prefix. Prefix will be
+                padded with trailing zeros if necessary. Must be positive. Defaults to 10.
+            prefix_count (int, optional): Number of unique prefixes to generate. If not provided,
+                world size will be used as default value. Defaults to None.
+
+        Raises:
+            ValueError: If epoch_num, min_prefix_length, or prefix_count are invalid.
         """
+        if min_prefix_length < 1:
+            raise ValueError(f"Minimum prefix length must be positive, got {min_prefix_length}")
+
+        if epoch_num is not None and not isinstance(epoch_num, int):
+            raise ValueError(f"Epoch number must be None or an integer, got {epoch_num}")
+
+        if prefix_count is not None and (not isinstance(prefix_count, int) or prefix_count < 1):
+                raise ValueError(f"Prefix count must be a positive integer, got {prefix_count}")
+
         super().__init__()
         self.base = base
         self.epoch_num = epoch_num
-        self.min_prefix_len = min_prefix_len
+        self.min_prefix_len = min_prefix_length
+
+        self.prefix_count = 1
+        if prefix_count is not None:
+            self.prefix_count = prefix_count
+        elif dist.is_initialized():
+            self.prefix_count = dist.get_world_size()
+
         self.prefix_map = self._generate_prefix_map()
 
     def generate_prefix(self, rank: int) -> str:
@@ -64,10 +88,7 @@ class NumericPrefixStrategy(S3PrefixStrategyBase):
 
     def _generate_prefix_map(self) -> List[str]:
         """Generate mapping of ranks to numeric-based prefixes."""
-        world_size = 1
-        if  dist.is_initialized():
-            world_size = dist.get_world_size()
-        minimum_required_length = self._calculate_prefix_length(world_size)
+        minimum_required_length = self._calculate_prefix_length()
         adjusted_prefix_length = max(minimum_required_length, self.min_prefix_len)
 
         all_prefixes = [
@@ -75,13 +96,15 @@ class NumericPrefixStrategy(S3PrefixStrategyBase):
             for i in range(self.base**minimum_required_length)
         ]
 
-        return all_prefixes[:world_size]
+        return all_prefixes[:self.prefix_count]
 
-    def _calculate_prefix_length(self, world_size: int) -> int:
+    def _calculate_prefix_length(self) -> int:
         """Calculate minimum prefix length needed for unique combinations."""
         prefix_length = 1
-        while self.base**prefix_length < world_size:
+        size = self.base
+        while size < self.prefix_count:
             prefix_length += 1
+            size *= self.base
         return prefix_length
 
     @abstractmethod
@@ -93,8 +116,8 @@ class NumericPrefixStrategy(S3PrefixStrategyBase):
 class BinaryPrefixStrategy(NumericPrefixStrategy):
     """Binary (Base2) prefix generation strategy using only 0 and 1."""
 
-    def __init__(self, epoch_num: int = None):
-        super().__init__(base=2, epoch_num=epoch_num)
+    def __init__(self, epoch_num: int = None, min_prefix_length: int = 10, prefix_count: int = None):
+        super().__init__(base=2, epoch_num=epoch_num, min_prefix_length=min_prefix_length, prefix_count=prefix_count)
 
     def _format_number(self, number: int, length: int) -> str:
         return format(number, f"0{length}b")
@@ -103,8 +126,8 @@ class BinaryPrefixStrategy(NumericPrefixStrategy):
 class HexPrefixStrategy(NumericPrefixStrategy):
     """Hexadecimal-based prefix generation strategy."""
 
-    def __init__(self, epoch_num: int = None):
-        super().__init__(base=16, epoch_num=epoch_num)
+    def __init__(self, epoch_num: int = None, min_prefix_length: int = 10, prefix_count: int = None):
+        super().__init__(base=16, epoch_num=epoch_num, min_prefix_length=min_prefix_length, prefix_count=prefix_count)
 
     def _format_number(self, number: int, length: int) -> str:
         return format(number, f"0{length}x")
