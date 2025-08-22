9e50b3d4426853c01be9d2c33145215853b3bcba
Mohamed Sharif <msharabdq@gmail.com>
2025-08-22 15:34:46 +0000

    dcp and fsdp save benchmark files are explicitly renamed as save, added --save flag to commands

--- s3torchbenchmarking/utils/run_dcp_ddp_benchmarks.sh
+++ s3torchbenchmarking/utils/run_dcp_ddp_benchmarks.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 #
-# Run PyTorch’s Distributed Checkpointing (DCP) benchmarks using DistributedDataParallel (DDP) training.
+# Run PyTorch's Distributed Checkpointing (DCP) benchmarks using DistributedDataParallel (DDP) training.
+# Usage:
+#   ./run_dcp_ddp_benchmarks.sh        # Run save benchmarks (default)
+#   ./run_dcp_ddp_benchmarks.sh --save  # Run save benchmarks (explicit)
+#   ./run_dcp_ddp_benchmarks.sh --load  # Run load benchmarks
 
 ./utils/run_benchmarks.sh -s dcp_ddp -d ./nvme/ "$@"
