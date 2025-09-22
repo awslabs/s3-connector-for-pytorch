a31d9cd90f124678dfffe2113d86d17dab50ad25
Mohamed Sharif <msharabdq@gmail.com>
2025-09-22 14:20:13 +0000

    Formatted test file

--- s3torchconnector/tst/unit/dcp/test_s3_storage_writer.py
+++ s3torchconnector/tst/unit/dcp/test_s3_storage_writer.py
@@ -9,10 +9,14 @@ TEST_BUCKET = "test-bucket"
 TEST_KEY = "test-key.txt"
 TEST_PATH = f"s3://{TEST_BUCKET}/{TEST_KEY}"
 
+
 @pytest.mark.parametrize("thread_count", [1, 2, 4, 8, 16])
 def test_s3storage_writer_thread_count(thread_count):
-    storage_writer = S3StorageWriter(region=TEST_REGION, path=TEST_PATH, thread_count=thread_count)
+    storage_writer = S3StorageWriter(
+        region=TEST_REGION, path=TEST_PATH, thread_count=thread_count
+    )
     assert storage_writer.thread_count == thread_count
+
 
 def test_s3storage_writer_thread_count_none_defaults_to_one():
     storage_writer = S3StorageWriter(region=TEST_REGION, path=TEST_PATH)
