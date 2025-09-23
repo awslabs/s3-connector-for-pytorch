0e8708971b0c142af3f8c71bb8202fcb25129dc4
Mohamed Sharif <msharabdq@gmail.com>
2025-09-23 14:43:25 +0000

    Refactored run_benchmarks script

--- s3torchbenchmarking/utils/run_benchmarks.sh
+++ s3torchbenchmarking/utils/run_benchmarks.sh
@@ -4,47 +4,31 @@
 
 set -euo pipefail
 
-# Check for --save and --load flags before getopts
-load_mode=false
-save_mode=false
-filtered_args=()
-for arg in "$@"; do
-  case $arg in
-    --load) load_mode=true ;;
-    --save) save_mode=true ;;
-    *) filtered_args+=("$arg") ;;
+# Parse flags and arguments
+mode="default"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --load) mode="load"; shift ;;
+    --save) mode="save"; shift ;;
+    -s) scenario="$2"; shift 2 ;;
+    -d) nvme_dir="$2"; shift 2 ;;
+    *) break ;;
   esac
 done
 
-# Set filtered arguments
-set -- "${filtered_args[@]}"
+# Prepare NVMe drive
+[[ -n ${nvme_dir:-} ]] && ./utils/prepare_nvme.sh "$nvme_dir"
 
-while getopts "s:d:" opt; do
-  case $opt in
-  s) scenario=$OPTARG ;; # name of the scenario
-  d) nvme_dir=$OPTARG ;; # mount point dir for saving checkpoints (will use NVMe drive)
-  *) ;;
-  esac
-done
+case $mode in
+  load) script="load_benchmark.py"; config="${scenario}_load" ;;
+  save) script="save_benchmark.py"; config="${scenario}_save" ;;
+  *) 
+    if [[ $scenario =~ ^dcp_(ddp|fsdp)$ ]]; then
+      echo "No flags detected, running DCP save benchmarks. To run DCP load benchmarks, use the --load flag"
+      script="save_benchmark.py"; config="${scenario}_save"
+    else
+      script="benchmark.py"; config="$scenario"
+    fi ;;
+esac
 
-shift $((OPTIND - 1)) # remove all processed positional arguments from "$@"
-
-# Prepare NVMe drive mount
-if [[ -n $nvme_dir ]]; then
-  ./utils/prepare_nvme.sh "$nvme_dir"
-fi
-
-
-# Run benchmarks; will write to DynamoDB table, if specified in the config (in `conf/aws/dynamodb.yaml`)
-# Use load_benchmark.py if -l flag is provided, otherwise use default benchmark.py
-if [[ "${load_mode:-}" == "true" ]]; then
-  python ./src/s3torchbenchmarking/"$scenario"/load_benchmark.py -cd conf -cn "${scenario}_load" +path="$nvme_dir" "$@"
-elif  [[ "${save_mode:-}" == "true" ]]; then
-  python ./src/s3torchbenchmarking/"$scenario"/save_benchmark.py -cd conf -cn "${scenario}_save" +path="$nvme_dir" "$@"
-elif [[ "$scenario" == "dcp_ddp" || "$scenario" == "dcp_fsdp" ]]; then
-  echo "No flags detected, running DCP save benchmarks. To run DCP load benchmarks, use the --load flag"
-  python ./src/s3torchbenchmarking/"$scenario"/save_benchmark.py -cd conf -cn "${scenario}_save" +path="$nvme_dir" "$@"
-else
-  python ./src/s3torchbenchmarking/"$scenario"/benchmark.py -cd conf -cn "$scenario" +path="$nvme_dir" "$@"
-fi
-
+python "./src/s3torchbenchmarking/$scenario/$script" -cd conf -cn "$config" +path="$nvme_dir" "$@"
