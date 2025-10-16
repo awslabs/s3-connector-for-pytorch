873f61999ed35da8d3e43df3caf5d3c0cd63344d
Jensen Tong <jetong@amazon.co.uk>
2025-10-16 18:18:58 +0100

    refactor: rename reader as dcp optimized and add max_gap_size support
    - Rename list of ranges / dcp list of ranges to DCP optimized
    - Allow max_gap_size to be passed through via S3ReaderConstructor
    
    Since the reader now does both list of ranges AND DCP optimisation by exploiting and requiring
    sequential access, I am renaming them to dcp optimized instead to better reflect its scope.

--- s3torchconnector/src/s3torchconnector/s3reader/__init__.py
+++ s3torchconnector/src/s3torchconnector/s3reader/__init__.py
@@ -2,10 +2,10 @@
 #  // SPDX-License-Identifier: BSD
 
 from .s3reader import S3Reader
-from .constructor import S3ReaderConstructor, DCPListOfRangesConstructor
+from .constructor import S3ReaderConstructor, DCPOptimizedConstructor
 from .sequential import SequentialS3Reader
 from .ranged import RangedS3Reader
-from .list_of_ranges import ListOfRangesS3Reader
+from .list_of_ranges import DCPOptimizedS3Reader, RangeRequest, RangeGroup
 from .protocol import GetStreamCallable, S3ReaderConstructorProtocol
 
 __all__ = [
@@ -13,5 +13,5 @@ __all__ = [
     "S3ReaderConstructor",
     "SequentialS3Reader",
     "RangedS3Reader",
-    "ListOfRangesS3Reader",
+    "DCPOptimizedS3Reader",
 ]
