5bc3a596e3f3c95648d275cdab1eb60091360e77
Jensen Tong <jetong@amazon.co.uk>
2025-10-20 22:58:37 +0100

    refactor: rename RangeRequest as ItemRange
    - Renaming to ItemRange since each represents the ranges of a ReadItem in PyTorch DCP LoadPlan
    - Minor docstring update / kwarg removals / comments

--- s3torchconnector/src/s3torchconnector/s3reader/constructor.py
+++ s3torchconnector/src/s3torchconnector/s3reader/constructor.py
@@ -8,21 +8,18 @@ from .s3reader import S3Reader
 from .protocol import S3ReaderConstructorProtocol
 from .sequential import SequentialS3Reader
 from .ranged import RangedS3Reader
-from .dcp_optimized import DCPOptimizedS3Reader, RangeRequest, DEFAULT_MAX_GAP_SIZE
+from .dcp_optimized import DCPOptimizedS3Reader, ItemRange, DEFAULT_MAX_GAP_SIZE
 
 
 class DCPOptimizedConstructor:
     def __init__(self, max_gap_size: int = DEFAULT_MAX_GAP_SIZE) -> None:
-        self._file_ranges: Dict[str, List[RangeRequest]] = {}
+        self._file_ranges: Dict[str, List[ItemRange]] = {}
         self._max_gap_size = max_gap_size
 
-    def set_file_ranges(self, file_ranges: Dict[str, List[RangeRequest]]) -> None:
+    def set_file_ranges(self, file_ranges: Dict[str, List[ItemRange]]) -> None:
         self._file_ranges = file_ranges
 
-    def __call__(
-        self, bucket: str, key: str, get_object_info, get_stream, **kwargs
-    ) -> S3Reader:
-        # TODO: use full key instead to cater for more general scenarios
+    def __call__(self, bucket: str, key: str, get_object_info, get_stream) -> S3Reader:
         filename = key.split("/")[-1]
         if self._file_ranges and filename in self._file_ranges:
             return DCPOptimizedS3Reader(
@@ -33,8 +30,8 @@ class DCPOptimizedConstructor:
                 get_stream=get_stream,
                 max_gap_size=self._max_gap_size,
             )
-        # Fallback to SequentialS3Reader if no file_ranges yet (e.g. when reading .metadata)
-        return SequentialS3Reader(bucket, key, get_object_info, get_stream, **kwargs)
+        # Fallback if file_ranges unavailable (e.g. when reading .metadata)
+        return SequentialS3Reader(bucket, key, get_object_info, get_stream)
 
 
 class S3ReaderConstructor:
@@ -110,8 +107,8 @@ class S3ReaderConstructor:
     def dcp_optimized(
         max_gap_size: int = DEFAULT_MAX_GAP_SIZE,
     ) -> S3ReaderConstructorProtocol:
-        """Creates a DCP-optimized constructor that uses DCPOptimizedS3Reader when ranges are available"""
-        # TODO update docstring
+        """Creates a DCPOptimizedConstructor that uses DCPOptimizedS3Reader when ranges are available"""
+        # TODO update docstring with guide and requirements to use this reader for DCP
         return DCPOptimizedConstructor(max_gap_size=max_gap_size)
 
     @staticmethod
